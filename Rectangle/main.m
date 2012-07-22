c1847fc56a5c62c6cea3fbf39e43a68d2c9c1254
Miene Lee <mlee@rhapsody.com>
2012-07-21 23:11:36 -0700

    Prog9.2 Illustration of Dynamic Typing & Binding

--- Rectangle/main.m
+++ Rectangle/main.m
@@ -13,34 +13,41 @@ int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
+        
+        // Illustrate Dynamic Typing & Binding
+        
+        id dataValue;
         Fraction *f1 = [[Fraction alloc] init];
-        Fraction *f2 = [[Fraction alloc] init];
-        Fraction *fracResult;
-        
         Complex *c1 = [[Complex alloc] init];
-        Complex *c2 = [[Complex alloc] init];
-        Complex *compResult;
         
-        [f1 setTo:1 over:10];
-        [f2 setTo:2 over:15];
+        [f1 setTo:2 over:5];
+        [c1 setReal:10.0 andImaginary:2.5];
         
-        [c1 setReal:18.0 andImaginary:2.5];
-        [c2 setReal:-5.0 andImaginary:3.2];
+        // first dataValue gets a fraction
         
-        // add and print 2 complex numbers
+        dataValue = f1;
+        [dataValue print];
         
-        [c1 print]; NSLog(@"         +"); [c2 print];
-        NSLog(@"-----------");
-        compResult = [c1 add:c2];
-        [compResult print];
-        NSLog(@"\n");
+        // now dataValue gets a complex number
         
-        // add and print 2 fractions
-        [f1 print]; NSLog(@"   +"); [f2 print];
-        NSLog(@"----");
-        [fracResult print];      
+        dataValue = c1;
+        [dataValue print];
         
+        BOOL test = [Fraction instancesRespondToSelector:@selector(setTo:over:)];
         
+        if(test){
+            NSLog(@"True");
+        }else {
+            NSLog(@"False");
+        }
+        
+        test = [f1 isMemberOfClass:[Fraction class]];
+        if(test){
+            NSLog(@"True");
+        }else {
+            NSLog(@"False");
+        }
+
                
     }
     return 0;
